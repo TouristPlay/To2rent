e3f28ebbcbea169b002dc2623a49597aeb6a8f44
Ilya Sergeev <touristtechology@gmail.com>
2020-05-01 16:57:42 +0300

    Основной Проект

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.slider').slick({
+        nextArrow: '<button class="slick__arrow slick_next"><img src="img/header/next.svg" alt="Next"></button>',
+        prevArrow: '<button class="slick__arrow slick_prev"><img src="img/header/prev.svg" alt="Prev"></button>'
+    });
+});
+
+
